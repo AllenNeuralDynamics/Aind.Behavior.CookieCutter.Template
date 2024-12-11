3c4f72d8692ec2f30ffc8eed803823bb9c8fd027
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-12-11 22:50:23 +0000

    Move transformation to json

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,5 @@
-from cookiecutter.main import cookiecutter
 from datetime import datetime
 
-repository_name = "{{ cookiecutter.repository_name }}"
-package_name = repository_name.replace(".", "_").lower()
-project_name = package_name.replace("_", "-")
-
-cookiecutter(
-    "cookiecutter-django",
-    extra_context={
-        "year": datetime.now().year,
-        "package_name": package_name,
-        "project_name": project_name,}
-)
+def on_pre_gen(context):
+    # Set the current year
+    context['cookiecutter']['year'] = datetime.now().year
