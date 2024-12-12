d46667d1ac5bf8926d8bc7bea3f4ebe1e54c9d39
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-12-12 07:47:08 +0000

    Add disabled code to export a model diagram

--- {{cookiecutter.repository_name}}/docs/conf.py
+++ {{cookiecutter.repository_name}}/docs/conf.py
@@ -10,14 +10,14 @@ import {{ cookiecutter.package_name }}.task_logic
 
 sys.path.insert(0, os.path.abspath("../src/DataSchemas"))
 
-SOURCE_ROOT = "https://github.com/AllenNeuralDynamics/Aind.Behavior.Device.Olfactometer/tree/main/src/DataSchemas/"
+SOURCE_ROOT = "https://github.com/AllenNeuralDynamics/{{ cookiecutter.repository_name }}/tree/main/src/DataSchemas/"
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "{{ cookiecutter.repository_name.replace('.', ' ') }} project"
 copyright = "{{ cookiecutter.year }}, Allen Institute for Neural Dynamics"
-author = "{{ cookiecutter.author }}"
+author = "{{ cookiecutter.author_name }}"
 release = {{ cookiecutter.package_name }}.__version__
 
 # -- General configuration ---------------------------------------------------
@@ -64,8 +64,6 @@ html_show_copyright = False
 
 
 # -- Options for linkcode extension ---------------------------------------
-
-
 def linkcode_resolve(domain, info):
     if domain != "py":
         return None
@@ -75,12 +73,11 @@ def linkcode_resolve(domain, info):
     return f"{SOURCE_ROOT}/{filename}.py"
 
 
-# -- Class diagram generation
-
-
+# -- Class diagram generation (Optional) ---------------------------------------
 def export_model_diagram(model: BaseModel, root: str = "_static") -> None:
     diagram = erd.create(model)
     diagram.draw(f"{root}/{model.__name__}.svg")
 
 
 _diagram_root = "_static"
+#export_model_diagram(MyPydanticBaseModel, _diagram_root)
